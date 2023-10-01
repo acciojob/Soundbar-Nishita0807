3a1245c401fbf64484019941ba40e85df1ada590
Nishita Rani <92663360+Nishita0807@users.noreply.github.com>
2023-10-01 17:05:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".btn");
+    const stopButton = document.querySelector(".stop");
+    const audio = document.getElementById("audio");
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", function () {
+            const soundFile = this.getAttribute("data-sound");
+            if (soundFile) {
+                audio.src = `sounds/${soundFile}`; // Assuming the music files are in an "album" folder
+                audio.play();
+            }
+        });
+    });
+
+    stopButton.addEventListener("click", function () {
+        audio.pause();
+        audio.currentTime = 0;
+    });
+});
